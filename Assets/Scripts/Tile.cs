b2e09fbc32f206c33018748f1fdbeafec4835f98
Alex <alexchang319@outlook.com>
2025-05-07 12:45:48 -0700

    Added wraparound system

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -8,28 +8,38 @@ public class Tile : MonoBehaviour
     public int y;
 
     private new GameObject gameObject;
-    private GameController gameController;
+    private GameController gc;
     private int hexCodeX;
     private int hexCodeY;
+    private GameObject[] things;
 
     // Main ---------------------------------------------------------------------
 
     void Start()
     {
-        doors = new bool[6];
-        adjacentTiles = new Tile[6];
-        gameObject = GameObject.FindWithTag("GameController");
-        gameController = gameObject.GetComponent<GameController>();
-        hexCodeX = (int)gameController.WorldPositionToHexPosition(transform.position.x, transform.position.y)[0];
-        hexCodeY = (int)gameController.WorldPositionToHexPosition(transform.position.x, transform.position.y)[1];
-        adjacentTiles[1] = gameController.grid[x, (y > gameController.gridWidth) ? 0 : y + 1];
-        adjacentTiles[4] = gameController.grid[x, (y == 0) ? gameController.gridHeight : y - 1];
+        doors= new bool[6];
+        gameObject=GameObject.FindWithTag("GameController");
+        gc=gameObject.GetComponent<GameController>();
+        InitializeAdjecentTiles();
     }
-
     // Accesors -----------------------------------------------------------------
 
     public Tile[] GetAjecentTiles(int x, int y)
     {
         return new Tile[0];
+    }
+
+    // Private -----------------------------------------------------------------
+
+    private void InitializeAdjecentTiles() {
+        adjacentTiles= new Tile[6];
+        hexCodeX=(int)gc.WorldPositionToHexPosition(transform.position.x,transform.position.y)[0];
+        hexCodeY=(int)gc.WorldPositionToHexPosition(transform.position.x,transform.position.y)[1];
+        adjacentTiles[1]=gc.grid[(x>gc.gridWidth-2)?0:x+1,y];
+        adjacentTiles[4]=gc.grid[(x==0)?gc.gridWidth-1:x-1,y];
+        adjacentTiles[0]=gc.grid[(y%2==0)?(x>gc.gridWidth-2)?0:x+1:x,(y>gc.gridHeight-2)?0:y+1];
+        adjacentTiles[2]=gc.grid[(y%2==0)?(x==0)?gc.gridWidth-1:x-1:x,(y==0)?gc.gridHeight-1:y-1];
+        adjacentTiles[3]=gc.grid[(y%2==0)?x:(x>gc.gridWidth-2)?0:x+1,(y==0)?gc.gridHeight-1:y-1];
+        adjacentTiles[5]=gc.grid[(y%2==0)?x:(x==0)?gc.gridWidth-1:x-1,(y>gc.gridHeight-2)?0:y+1];
     }
 }
